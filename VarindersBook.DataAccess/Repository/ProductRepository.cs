ad34b91a45a8e6c37014385418db368a1ecb60e1
Varinder Kaur <VK92@myscc.ca>
2022-11-24 15:08:30 -0500

    updated the files

---
+++ VarindersBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VarindersBook.DataAccess.Repository.IRepository;
+using VarindersBook.DataAccess.Repository;
+using VarindersBook.Models;
+using VarindersBook.DataAccess.Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace VarindersBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(s => s.Id == product.Id);
+
+            if (objFromDb != null)
+            {
+                if (product.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = product.ImageUrl;
+                }
+                objFromDb.Title = product.Title;
+                objFromDb.Description = product.Description;
+                objFromDb.ISBN = product.ISBN;
+                objFromDb.Author = product.Author;
+                objFromDb.ListPrice = product.ListPrice;
+                objFromDb.CategoryId = product.CategoryId;
+                objFromDb.CoverTypeId = product.CoverTypeId;
+
+
+            }
+        }
+    }
+}
